4d2af9cb3cec22feb2007ef45c839576e8e6b3f0
Esther Lubas <estherzulema10@gmail.com>
2019-11-16 21:05:30 -0200

    Adicionando a listagem ainda crua dos itens adicionados na lista

--- lista-compras/src/services/ListaService.js
+++ lista-compras/src/services/ListaService.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import itens from './itens';
+
+const LIMITE_DE_ITENS_RETORNADOS = 5;
 
 export default class ListaService {
 
     constructor() {
         this.listas = [];
         this.api = axios.create({
-            baseURL: 'https://secret-plateau-95576.herokuapp.com/listas'
+            baseURL: 'http://localhost:3001/listas'
         });
     }
 
@@ -17,6 +20,31 @@ export default class ListaService {
 
     async salvar(lista) {
         await this.api.post('/', lista);
+    }
+
+    recuperarItens(termo) {
+        /**
+         * Se não tem nenhum termo,
+         * ou seja, nenhum filtro de consulta,
+         * então retorna uma lista vazia.
+         */
+        if (!termo) {
+            return [];
+        }
+
+        // Converte para minúsculo
+        termo = termo.toLowerCase();
+        // Função de filtragem
+        let itensFiltrados = itens.filter(item => {
+            let descricao = item.descricao.toLowerCase();
+            return descricao.includes(termo);
+        });
+
+        if (itensFiltrados.length > LIMITE_DE_ITENS_RETORNADOS) {
+            return itensFiltrados.slice(0, LIMITE_DE_ITENS_RETORNADOS);
+        }
+
+        return itensFiltrados;
     }
 
 }
